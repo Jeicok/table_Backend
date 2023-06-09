bd2bc97d25b78b1655e3d52ad197a447368bc38d
jeicok <jeyo_@hotmail.com>
2023-06-09 04:56:33 -0500

    Se agrega el proyecto

---
+++ HurtadoBackend/Modelos/Genero.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HurtadoBackend.Modelos
+{
+    public class Genero
+    {
+        public int id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public String GnrNombre { get; set; }
+        public Boolean GnrEstado { get; set; }
+    }
+}
---
+++ HurtadoBackend/Modelos/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HurtadoBackend.Modelos
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public String UsrNombre { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public String UsrClave { get; set; }
+
+        public Boolean UsrEstado { get; set; }
+
+        //////////////////////////////////////////////
+
+        public Genero Genero { get; set; }
+        public int GeneroId { get; set; }
+    }
+}
